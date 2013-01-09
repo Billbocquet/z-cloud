8945fc7a9cb84f60f10c55b9c6771feeca6de59b
jussiva <jussiva@gmail.com>
2013-01-09 20:08:59 +0200

    fixed Certification folders from Run_Z-Agent.sh.

--- Z-Connector/Unix/Run_Z-Agent.sh
+++ Z-Connector/Unix/Run_Z-Agent.sh
@@ -9,7 +9,7 @@ fi
 
 while [ 1 ]
 do
-	./z-agent -s z-cloud.z-wave.me --cacert ../Certificates/cacert.pem --cert ../Certificates/cert.pem --key ../Certificates/cert.key -d /dev/ttyUSB0 "${@}"
+	./z-agent -s z-cloud.z-wave.me --cacert Certificates/cacert.pem --cert Certificates/cert.pem --key Certificates/cert.key -d /dev/ttyUSB0 "${@}"
 	# substitute /dev/ttyUSB0 by /dev/ttyACM0 or another device depending on the hardware you have
 	# on Mac OS X use /dev/cu.usbserial or another cu.* device
 
